c3bf1f6534bf9a53b8c81bff69b388988559911b
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2020-07-24 20:06:30 -0400

    Reformat Example.java

--- Example.java
+++ Example.java
@@ -13,7 +13,8 @@ public class Example {
     public static int count = 100;
     public static int bucketCount = 10;
     public static String input_file = DEFAULT_INPUT_FILE;
-	public static void main(String[] args) {
+
+    public static void main(String[] args) {
 
         if (args.length >= 1)
             count = Integer.parseInt(args[0]);
@@ -21,26 +22,26 @@ public class Example {
             bucketCount = Integer.parseInt(args[1]);
         if (args.length >= 3)
             input_file = args[2];
-        
+
         RandomHashFamily rh = new RandomHashFamily(count);
         UniformAudit ua = new UniformAudit(bucketCount, RandomHashFamily.MIN_VALUE, RandomHashFamily.MAX_VALUE);
 
-		BufferedReader reader;
-		try {
-			reader = new BufferedReader(new FileReader(input_file));
-            
-            String line = null;  
-            while ((line = reader.readLine()) != null) { 
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(input_file));
+
+            String line = null;
+            while ((line = reader.readLine()) != null) {
                 long[] hashes = rh.hashes(line);
-                /*System.out.println(line);
-                System.out.println(hash);
-                System.out.println();*/
-                for(int i=0; i < count; i++)
+                /*
+                 * System.out.println(line); System.out.println(hash); System.out.println();
+                 */
+                for (int i = 0; i < count; i++)
                     ua.update(hashes[i]);
             }
-			reader.close();
-		} catch (IOException e) {
-			e.printStackTrace();
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
         System.out.println("input: " + input_file);
